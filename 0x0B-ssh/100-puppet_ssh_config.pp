3640834b2aed2bf9037280d82a43a39f79073b79
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-01-04 10:00:55 +0300

    Added task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,7 +4,7 @@ include stdlib
 file_line { 'Turn off passwd auth':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => '    BatchMode yes',
+  line   => '    PasswordAuthentication no',
   replace => true,
 }
 
