f56a30da2284c75b420abc98c95b3eeae7601338
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-02-15 16:46:53 +0300

    Added tasks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,10 +5,10 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Finds the total number of subscribers."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Client"}
+    url = "https://www.reddit.com/r/{}/about".format(subreddit)
+    headers = {"User-Agent": "My-User-Agent"}
     res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code != 200:
+    if res.status_code >= 300:
         return 0
     data = res.json().get("data")
     return data.get("subscribers")
